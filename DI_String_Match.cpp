c845dd4eae11b8b08ce6f3e4affd983cef7ef8a0
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-06-10 03:27:54 -0500

    Update and rename DI_String_Match to DI_String_Match.cpp

--- DI_String_Match.cpp
+++ DI_String_Match.cpp
@@ -1,17 +1,18 @@
 /* A permutation perm of n + 1 integers of all the integers in the range [0, n] can be represented as a string s of length n where:
-
 s[i] == 'I' if perm[i] < perm[i + 1], and
 s[i] == 'D' if perm[i] > perm[i + 1].
 Given a string s, reconstruct the permutation perm and return it. If there are multiple valid permutations perm, return any of them.*/
 
-class Solution {
-public:
-    vector<int> diStringMatch(string s) {
-        
-    vector<int> res;
-        
-    for (int l = 0, i = 0, h = s.size(); i <= s.size(); ++i)
-        res.push_back(i == s.size() || s[i] == 'I' ? l++ : h--);
-    return res;
-} 
+class Solution 
+{
+    public:
+        vector<int> diStringMatch(string s) 
+        {
+            vector<int> res;
+
+            for (int l = 0, i = 0, h = s.size(); i <= s.size(); ++i)
+                res.push_back(i == s.size() || s[i] == 'I' ? l++ : h--);
+                
+            return res;
+        } 
 };
