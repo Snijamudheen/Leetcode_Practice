3d263c54f55e13a721bc3da2c19fe46fa907d9ae
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-06-24 04:46:47 -0500

    Update and rename hash_table to hash_table.py

--- hash_table.py
+++ hash_table.py
@@ -1,7 +1,7 @@
-#Python program that searches through a hashtable for a specific key word. It further indicates the key's index position if key is found.
+# Python program that searches through a hashtable for a specific keyword. It further indicates the key's index position if key is found.
 
 def hash_find(key, hashtable):
-    if(hashtable[key%10] == key):
+    if(hashtable[key % 10] == key):
         return ((key % 10) + 1)
     else:
         return None
@@ -11,19 +11,19 @@ print("We created a hashtable of 10 positions: ")
 print(hashtable)
 
 L = [34,23,12,45,67,4,5]
-print("the given list is ", L[::])
+print("The given list is ", L[::])
 
 for i in range(0, len(L)):
     hashtable[L[i] % 10] = L[i]
 print("The hashtable contents are: ")
 
-for i in range(0,len(hashtable)):
-    print("hashindex = ", i,"  ,value = ",hashtable[i])
+for i in range(0, len(hashtable)):
+    print("hashindex = ", i, "  ,value = ", hashtable[i])
     
 key = int(input("Enter the number to be searched: "))
 pos = hash_find(key, hashtable)
 
 if pos is None:
-    print("Number", key," is not present in the hash table.")
+    print("Number", key, " is not present in the hash table.")
 else:
-    print("number ", key,"is present in the hash table at position ",pos)
+    print("Number", key, " is present in the hash table at position ", pos, ".")
