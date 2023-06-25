e2472771b35eebd92a73533f0c859056b1982357
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-06-25 16:31:47 -0500

    Update and rename length_of_last_word to length_of_last_word.c

---
+++ length_of_last_word.c
@@ -0,0 +1,22 @@
+/*Given a string s consisting of words and spaces, return the length of the last word in the string. A word is a maximal substring consisting of non-space characters only.*/
+
+class Solution 
+{
+    public:
+        int lengthOfLastWord(string s) 
+        {
+            int i = s.length() - 1;
+            int count = 0;
+            
+            while(s[i] == ' ')    
+                i--;
+            
+            while(i > -1  && s[i] != ' ')
+            { 
+                count++;
+                i--;
+            }
+            
+            return count;
+        }
+};
