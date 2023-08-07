921235bdea3d402ee685a466a1081a1d9de6e16b
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-08-07 01:25:46 -0500

    Create Arranging_Coins.c

---
+++ Arranging_Coins.c
@@ -0,0 +1,25 @@
+/*You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. 
+The last row of the staircase may be incomplete.
+Given the integer n, return the number of complete rows of the staircase you will build.*/
+
+class Solution 
+{
+  public:
+      int arrangeCoins(int n) 
+      {
+          int c = n;
+          long long i = 1;
+
+          if(n == 0) 
+              return 0;
+          else if(n == 1) 
+              return 1;
+
+          while(c > 0)
+          {
+              i++;
+              c -= i;
+          }
+          return i - 1;
+      }
+};
