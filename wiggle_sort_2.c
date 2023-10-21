dac85e146ed96ba8ef174ffd6d607794132d33b9
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-10-21 17:22:59 -0500

    Update wiggle_sort_2.c

--- wiggle_sort_2.c
+++ wiggle_sort_2.c
@@ -1,3 +1,27 @@
 /*Given an integer array nums, reorder it such that nums[0] < nums[1] > nums[2] < nums[3]....
 You may assume the input array always has a valid answer.*/
 
+void wiggleSort(int* nums, int numsSize){
+      int arr[5001];
+      for(int i=0;i<5001;i++) arr[i]=0;
+      for(int i=0;i<numsSize;i++){
+          arr[nums[i]]++;
+      }
+      int end=5000;
+      int in=1;   //start to  fill largest value from 1 index
+       while(in<numsSize && end>-1){
+          if(arr[end]==0){ end--; continue;}
+          else{  nums[in]=end;
+                 in+=2;
+                 arr[end]--;
+          }
+      }
+      in=0; // start to fill next set of values from index 0
+        while(in<numsSize && end>-1){
+          if(arr[end]==0){ end--; continue;}
+          else{  nums[in]=end;
+                 in+=2;
+                 arr[end]--;
+          }
+      }
+}
