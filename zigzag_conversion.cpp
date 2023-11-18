81b7edc53502d9930675712843a3b6a4c8d8c6cf
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-11-18 01:32:36 -0600

    Update zigzag_conversion.cpp

--- zigzag_conversion.cpp
+++ zigzag_conversion.cpp
@@ -5,3 +5,35 @@ Y   I   R
 And then read line by line: "PAHNAPLSIIGYIR"
 Write the code that will take a string and make this conversion given a number of rows:
 string convert(string s, int numRows);*/
+
+class Solution {
+public:
+
+    string convert(string s, int numRows) {
+    
+    if(numRows <= 1) return s;
+
+    vector<string>v(numRows, ""); 
+
+    int j = 0, dir = -1;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+        if(j == numRows - 1 || j == 0) dir *= (-1); 
+		 
+        v[j] += s[i];
+
+        if(dir == 1) j++;
+
+        else j--;
+    }
+
+    string res;
+
+    for(auto &it : v) res += it; 
+
+    return res;
+
+    }
+};
