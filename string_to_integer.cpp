f5604a12ec91b341e1ebd4a4a3ce42456470dfc3
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-11-23 22:28:53 -0600

    Update string_to_integer.cpp

--- string_to_integer.cpp
+++ string_to_integer.cpp
@@ -12,35 +12,44 @@ Note:
 Only the space character ' ' is considered a whitespace character.
 Do not ignore any characters other than the leading whitespace or the rest of the string after the digits.*/
 
-class Solution {
- public:
-  int myAtoi(string s) {
-    trim(s);
-    if (s.empty())
-      return 0;
-
-    const int sign = s[0] == '-' ? -1 : 1;
-    if (s[0] == '+' || s[0] == '-')
-      s = s.substr(1);
-
-    long num = 0;
-
-    for (const char c : s) {
-      if (!isdigit(c))
-        break;
-      num = num * 10 + (c - '0');
-      if (sign * num < INT_MIN)
-        return INT_MIN;
-      if (sign * num > INT_MAX)
-        return INT_MAX;
-    }
-
-    return sign * num;
-  }
-
- private:
-  void trim(string& s) {
-    s.erase(0, s.find_first_not_of(' '));
-    s.erase(s.find_last_not_of(' ') + 1);
-  }
+class Solution 
+{
+  public:
+   int myAtoi(string s) 
+   {
+     trim(s);
+    
+     if (s.empty())
+       return 0;
+ 
+     const int sign = s[0] == '-' ? -1 : 1;
+    
+     if (s[0] == '+' || s[0] == '-')
+       s = s.substr(1);
+ 
+     long num = 0;
+ 
+     for (const char c : s) 
+     {
+       if (!isdigit(c))
+         break;
+      
+       num = num * 10 + (c - '0');
+      
+       if (sign * num < INT_MIN)
+         return INT_MIN;
+      
+       if (sign * num > INT_MAX)
+         return INT_MAX;
+     }
+ 
+     return sign * num;
+   }
+ 
+  private:
+   void trim(string& s)
+   {
+     s.erase(0, s.find_first_not_of(' '));
+     s.erase(s.find_last_not_of(' ') + 1);
+   }
 };
