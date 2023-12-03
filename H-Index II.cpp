3965e728b35bb6ecfb6f46bcaa2b3bc4a4cbe07d
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-12-03 04:01:10 -0600

    Update H-Index II.cpp

--- H-Index II.cpp
+++ H-Index II.cpp
@@ -2,22 +2,29 @@
 According to the definition of h-index on Wikipedia: The h-index is defined as the maximum value of h such that the given researcher has published at least h papers that have each been cited at least h times.
 You must write an algorithm that runs in logarithmic time.*/
 
-class Solution {
-public:
-    int hIndex(vector<int>& citations) {
-        int n=citations.size();
-        int low=0, high=n-1;
-        int ans=0;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(citations[mid]>=n-mid){
-                ans=max(ans, n-mid);
-                high=mid-1;
+class Solution 
+{
+    public:
+        int hIndex(vector<int>& citations) 
+        {
+            int n = citations.size();
+            int low = 0, high = n - 1;
+            int ans = 0;
+            
+            while(low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+                if(citations[mid] >= n - mid)
+                {
+                    ans = max(ans, n - mid);
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
             }
-            else{
-                low=mid+1;
-            }
+            return ans;
         }
-        return ans;
-    }
 };
