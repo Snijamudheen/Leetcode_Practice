3457bd0e7c74920049404709f066ecc78c2be66d
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-12-11 01:54:53 -0600

    Update candy.cpp

--- candy.cpp
+++ candy.cpp
@@ -4,3 +4,21 @@ Each child must have at least one candy.
 Children with a higher rating get more candies than their neighbors.
 Return the minimum number of candies you need to have to distribute the candies to the children.
  */
+
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        vector<int> candy(ratings.size(), 1);
+        for(int i = 1; i < ratings.size(); i++) {
+            if(ratings[i] > ratings[i - 1]) {
+                candy[i] = candy[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.size() - 2; i > -1; i--) {
+            if(ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
+                candy[i] = candy[i + 1] + 1;
+            } 
+        }
+        return accumulate(candy.begin(), candy.end(), 0);
+    }
+};
