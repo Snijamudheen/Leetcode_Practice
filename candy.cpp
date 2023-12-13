29079fc6e7fd3607826f944ad31b58812cd3fd75
Shaheen Nijamudheen <67351194+Snijamudheen@users.noreply.github.com>
2023-12-12 23:29:27 -0600

    Update candy.cpp

--- candy.cpp
+++ candy.cpp
@@ -5,20 +5,28 @@ Children with a higher rating get more candies than their neighbors.
 Return the minimum number of candies you need to have to distribute the candies to the children.
  */
 
-class Solution {
-public:
-    int candy(vector<int>& ratings) {
-        vector<int> candy(ratings.size(), 1);
-        for(int i = 1; i < ratings.size(); i++) {
-            if(ratings[i] > ratings[i - 1]) {
-                candy[i] = candy[i - 1] + 1;
+class Solution 
+{
+    public:
+        int candy(vector<int>& ratings) 
+        {
+            vector<int> candy(ratings.size(), 1);
+            
+            for(int i = 1; i < ratings.size(); i++) 
+            {
+                if(ratings[i] > ratings[i - 1]) 
+                {
+                    candy[i] = candy[i - 1] + 1;
+                }
             }
+            
+            for(int i = ratings.size() - 2; i > -1; i--) 
+            {
+                if(ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) 
+                {
+                    candy[i] = candy[i + 1] + 1;
+                } 
+            }
+            return accumulate(candy.begin(), candy.end(), 0);
         }
-        for(int i = ratings.size() - 2; i > -1; i--) {
-            if(ratings[i] > ratings[i + 1] && candy[i] <= candy[i + 1]) {
-                candy[i] = candy[i + 1] + 1;
-            } 
-        }
-        return accumulate(candy.begin(), candy.end(), 0);
-    }
 };
