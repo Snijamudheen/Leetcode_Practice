f599e2e442258a15a6b200af8f38d4c9eedc6a3b
Sha <67351194+Snijamudheen@users.noreply.github.com>
2024-03-29 01:03:01 -0500

    Update Longest_Palindromic_Substring.py

--- python/Longest_Palindromic_Substring.py
+++ python/Longest_Palindromic_Substring.py
@@ -9,3 +9,31 @@ Example 2:
 Input: s = "cbbd"
 Output: "bb"   '''
 
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        reslen = 0
+
+        
+        for i in range (len(s)):
+            l, r = i, i
+            while l>=0 and r < len(s) and s[l] == s[r]:
+                #odd length
+                if(r - l + 1) > reslen:
+                    res = s[l:r + 1]
+                    reslen = r - l + 1
+
+                l -= 1
+                r += 1
+
+            l, r = i, i + 1
+            while l>=0 and r < len(s) and s[l] == s[r]:
+                #even length
+                if(r - l + 1) > reslen:
+                    res = s[l:r + 1]
+                    reslen = r - l + 1
+
+                l -= 1
+                r += 1
+
+        return res
