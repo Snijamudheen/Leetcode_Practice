06e7fdf4a8f38e6bef2d7eb86001b736ae9ea4b8
Sha <67351194+Snijamudheen@users.noreply.github.com>
2025-07-20 17:48:16 -0500

    Create Palindrome Linked List.py

---
+++ python/mine/Palindrome Linked List.py
@@ -0,0 +1,22 @@
+# Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        list_val = []
+
+        while head:
+            list_val.append(head.val)
+            head = head.next
+
+        left, right = 0, len(list_val) - 1
+
+        while left < right and list_val[left] == list_val[right]:
+            left += 1
+            right -= 1
+
+        return left >= right 
